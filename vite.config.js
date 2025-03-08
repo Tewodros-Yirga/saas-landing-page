ab63dabef24553b1fb00a647387da05d488c3050
Tewodros Yirga <tewodrosy21@gmail.com>
2025-03-08 19:10:53 +0300

    Vite configured

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sass-landing-page/', // Set the base path for GitHub Pages
+  base: '/saas-landing-page/', 
 });
